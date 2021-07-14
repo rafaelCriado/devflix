32911b6ba04d2966640b05b6e2e3ab93b179595e
Rafael Criado <rafael.marquescriado@gmail.com>
2021-07-14 16:20:16 -0300

    Adiciona modal e trailers das series

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var filmes = [
     { 
       "title": "Breaking Bad", 
+      "urlVideo":"https://youtube.com/embed/W6_6gJda0", 
       "img": "img/thumbs/breakingbad.jpg", 
       "imgCapa": "img/capas/breakingbad.jpg", 
       "description":"Ao saber que tem câncer, um professor passa a fabricar metanfetamina pelo futuro da família, mudando o destino de todos." , 
@@ -12,6 +13,7 @@ var filmes = [
     },
     { 
       "title": "Elite", 
+      "urlVideo":"https://youtube.com/embed/t04JpO7GbYc", 
       "img": "img/thumbs/elite.jpg", 
       "imgCapa": "img/capas/elite.jpg",
       "description":"Após três jovens da escola pública serem transferidos para um conceituado colégio de elite, o conflito entre classes acaba levando a um assassinato." , 
@@ -23,7 +25,7 @@ var filmes = [
     },
     { 
       "title": "Flash", 
-      "img": "img/thumbs/flash.jpg", 
+      "urlVideo":"https://youtube.com/embed/p5SR_996zwM", "img": "img/thumbs/flash.jpg", 
       "imgCapa": "img/capas/flash.jpg",
       "description":"Um perito forense desperta de um coma com poderes especiais que serão postos à prova na luta contra forças que ameaçam a cidade." , 
       "temporadas": [
@@ -34,7 +36,7 @@ var filmes = [
     },
     { 
       "title": "Game of Thrones", 
-      "img": "img/thumbs/gameofthrones.jpg", 
+      "urlVideo":"https://youtube.com/embed/KPLWWIOCOOQ", "img": "img/thumbs/gameofthrones.jpg", 
       "imgCapa": "img/capas/gameofthrones.jpg",
       "description":"Em uma terra onde os verões podem durar vários anos e o inverno toda uma vida, sete nobres famílias lutam pelo controle da mítica terra de Westeros, dividida depois de uma guerra. Num cenário que lembra a Europa medieval, reis, rainhas, cavaleiros e renegados usam todos os meios possíveis em um jogo político pela disputa do Trono de Ferro, o símbolo do poder absoluto." , 
       "temporadas": [
@@ -45,7 +47,7 @@ var filmes = [
     },
     { 
       "title": "The Good Doctor", 
-      "img": "img/thumbs/gooddoctor.jpg", 
+      "urlVideo":"https://youtube.com/embed/fYlZDTru55g", "img": "img/thumbs/gooddoctor.jpg", 
       "imgCapa": "img/capas/gooddoctor.jpg",
       "description":"Um jovem médico com autismo começa a trabalhar em um famoso hospital. Além dos desafios da profissão, ele terá também que provar sua capacidade a seus colegas e superiores." , 
       "temporadas": [
@@ -56,7 +58,7 @@ var filmes = [
     },
     { 
       "title": "Dr. House", 
-      "img": "img/thumbs/house.jpg", 
+      "urlVideo":"https://youtube.com/embed/1nqW4g_S0wo", "img": "img/thumbs/house.jpg", 
       "imgCapa": "img/capas/house.jpg",
       "description":"O cientista House (Hugh Laurie) é especializado em infectologia e nefrologia, realiza excelentes diagnósticos mantendo uma postura cética, distanciada e até mesmo mau humorada com os pacientes do fictício hospital de Princeton-Plainsboro." , 
       "temporadas": [
@@ -67,6 +69,7 @@ var filmes = [
     },
     { 
       "title": "Lei & Ordem", 
+      "urlVideo":"https://youtube.com/embed/Tc4FDG9Kbxg", 
       "img": "img/thumbs/leieordem.jpg", 
       "imgCapa": "img/capas/leieordem.jpg",
       "description":"Detetives que fazem parte da Unidade de Vítimas Especiais da polícia de Nova York investigam crimes de natureza sexual, como estupros, em que a vítima sobrevive, e auxilia as autoridades na investigação." , 
@@ -78,7 +81,7 @@ var filmes = [
     },
     { 
       "title": "Loki", 
-      "img": "img/thumbs/loki.jpg", 
+      "urlVideo":"https://youtube.com/embed/xdBWrkcmMwU", "img": "img/thumbs/loki.jpg", 
       "imgCapa": "img/capas/loki.jpg", 
       "description":"Começando imediatamente depois que Loki rouba o Tesseract (de novo), ele se encontra diante da Autoridade de Variação Temporal, uma organização burocrática que existe fora do tempo e espaço. Forçado a responder por seus crimes contra a linha do tempo, ele recebe uma escolha: Ser deletado da realidade ou ajudar a capturar uma ameaça ainda maior." , 
       "temporadas": [
@@ -89,7 +92,7 @@ var filmes = [
     },
     { 
       "title": "Lucifer", 
-      "img": "img/thumbs/lucifer.jpg", 
+      "urlVideo":"https://youtube.com/embed/YYu_NIKpU-8", "img": "img/thumbs/lucifer.jpg", 
       "imgCapa": "img/capas/lucifer.jpg",
       "description":"Entediado com a vida nas trevas, o diabo se muda para Los Angeles, abre um piano-bar e empresta sua sabedoria a uma investigadora de assassinatos." , 
       "temporadas": [
@@ -100,7 +103,7 @@ var filmes = [
     },
     { 
       "title": "Peaky Blinders", 
-      "img": "img/thumbs/peaky.jpg", 
+      "urlVideo":"https://youtube.com/embed/oVzVdvGIC7U", "img": "img/thumbs/peaky.jpg", 
       "imgCapa": "img/capas/peaky.jpg",
       "description":"Thomas Shelby e seus irmãos retornam a Birmingham depois de servir no exército britânico durante a Primeira Guerra Mundial. Os Peaky Blinders, a gangue na qual Thomas é líder, controlam a cidade de Birmingham. Mas, como as ambições de Shelby se estendem para além de Birmingham, ele planeja construir o império de negócios que criou e impedir qualquer um que atrapalhar." , 
       "temporadas": [
@@ -111,7 +114,7 @@ var filmes = [
     },
     { 
       "title": "Vikings", 
-      "img": "img/thumbs/vikings.jpg", 
+      "urlVideo":"https://youtube.com/embed/dV6p83HRk4o", "img": "img/thumbs/vikings.jpg", 
       "imgCapa": "img/capas/vikings.jpg",
       "description":"Ragnar Lothbrok, um jovem que se considera descendente de Odin, é um guerreiro, fazendeiro e chefe de família. Visionário e sonhando viver diversas aventuras, ele acredita que as riquezas sonhadas por seu povo encontram-se além do mar Báltico. Mas para conseguir conquistá-las ele precisa convencer Earl Haraldson, o líder de seu povo, a permitir que ele e seus guerreiros viagem em direção à Inglaterra." , 
       "temporadas": [
@@ -122,6 +125,10 @@ var filmes = [
     }
 ];
 
+const btn = document.getElementById('btnAssistirAgora');
+const btnInfo = document.getElementById('btnMaisInformacao');
+
+
 function carregaPrincipal(index){
     document.querySelector(".filme-principal .titulo").innerHTML = filmes[index].title;
     document.querySelector(".filme-principal .descricao").innerHTML = filmes[index].description;
@@ -130,6 +137,8 @@ function carregaPrincipal(index){
     document.querySelector(".filme-principal").style.backgroundSize = `cover`;
     document.querySelector(".filme-principal").style.backgroundPosition = `0%0%`;  
     document.querySelector(".filme-principal").style.transition = `.8s ease all`;
+    btn.dataset.index = index;
+    btnInfo.dataset.index = index;
 }
 
 $('.owl-carousel').owlCarousel({
@@ -159,10 +168,36 @@ filmes.forEach( (filme, index) => {
 })
 
 
-const btn = document.getElementById('btnAssistirAgora');
+
 
 btn.addEventListener('click', ()=>{
-    alert('oi')
+    // alert(btn.dataset.index);
+    document.getElementById("youtubeIFrame").src = filmes[btn.dataset.index].urlVideo;
+})
+
+btnInfo.addEventListener('click', ()=>{
+    let filme = filmes[btnInfo.dataset.index];
+
+    let bodyModalInfo = document.getElementById('infoBody');
+    bodyModalInfo.innerHTML = '';
+
+    let titulo = document.createElement('h4');
+    titulo.classList.add('titulo');
+    titulo.innerHTML = filme.title;
+
+    let descricao = document.createElement('p');
+    descricao.classList.add('descricao');
+    descricao.innerHTML = filme.description;
+
+    let temporadas = document.createElement('p');
+    temporadas.classList.add('temporada');
+    temporadas.innerHTML = `TEMPORADAS: ${filme.temporadas.length}`;
+
+    bodyModalInfo.appendChild(titulo);
+    bodyModalInfo.appendChild(descricao);
+    bodyModalInfo.appendChild(temporadas);
+    
+
 })
 
 function getRandomInt(max) {
---
+++ js/modal.js
@@ -0,0 +1,2 @@
+document.addEventListener('click',function(e){e=e||window.event;var target=e.target||e.srcElement;e.preventDefault();if(target.hasAttribute('data-toggle')&&target.getAttribute('data-toggle')=='modal'){if(target.hasAttribute('data-target')){var m_ID=target.getAttribute('data-target');document.getElementById(m_ID).classList.add('open');}}
+if((target.hasAttribute('data-dismiss')&&target.getAttribute('data-dismiss')=='modal')||target.classList.contains('modal')){var modal=document.querySelector('[class="modal open"]');modal.classList.remove('open');}},false);
