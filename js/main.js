f4e32254e4fb21832c136c6139dd5abe0bdbab55
Rafael Criado <rafael.marquescriado@gmail.com>
2021-07-14 15:15:51 -0300

    Séries são carregadas dinamicamente por json

--- js/main.js
+++ js/main.js
@@ -1,4 +1,139 @@
+var filmes = [
+    { 
+      "title": "Breaking Bad", 
+      "img": "img/thumbs/breakingbad.jpg", 
+      "imgCapa": "img/capas/breakingbad.jpg", 
+      "description":"Ao saber que tem câncer, um professor passa a fabricar metanfetamina pelo futuro da família, mudando o destino de todos." , 
+      "temporadas": [
+        { "numeroTemporada": 1, "description": "", "qtdeEpisodios": 9 },
+        { "numeroTemporada": 2, "description": "", "qtdeEpisodios": 10 },
+        { "numeroTemporada": 3, "description": "", "qtdeEpisodios": 11 }
+      ]
+    },
+    { 
+      "title": "Elite", 
+      "img": "img/thumbs/elite.jpg", 
+      "imgCapa": "img/capas/elite.jpg",
+      "description":"Após três jovens da escola pública serem transferidos para um conceituado colégio de elite, o conflito entre classes acaba levando a um assassinato." , 
+      "temporadas": [
+        { "numeroTemporada": 1, "description": "", "qtdeEpisodios": 9 },
+        { "numeroTemporada": 2, "description": "", "qtdeEpisodios": 10 },
+        { "numeroTemporada": 3, "description": "", "qtdeEpisodios": 11 }
+      ]
+    },
+    { 
+      "title": "Flash", 
+      "img": "img/thumbs/flash.jpg", 
+      "imgCapa": "img/capas/flash.jpg",
+      "description":"Um perito forense desperta de um coma com poderes especiais que serão postos à prova na luta contra forças que ameaçam a cidade." , 
+      "temporadas": [
+        { "numeroTemporada": 1, "description": "", "qtdeEpisodios": 9 },
+        { "numeroTemporada": 2, "description": "", "qtdeEpisodios": 10 },
+        { "numeroTemporada": 3, "description": "", "qtdeEpisodios": 11 }
+      ]
+    },
+    { 
+      "title": "Game of Thrones", 
+      "img": "img/thumbs/gameofthrones.jpg", 
+      "imgCapa": "img/capas/gameofthrones.jpg",
+      "description":"Em uma terra onde os verões podem durar vários anos e o inverno toda uma vida, sete nobres famílias lutam pelo controle da mítica terra de Westeros, dividida depois de uma guerra. Num cenário que lembra a Europa medieval, reis, rainhas, cavaleiros e renegados usam todos os meios possíveis em um jogo político pela disputa do Trono de Ferro, o símbolo do poder absoluto." , 
+      "temporadas": [
+        { "numeroTemporada": 1, "description": "", "qtdeEpisodios": 9 },
+        { "numeroTemporada": 2, "description": "", "qtdeEpisodios": 10 },
+        { "numeroTemporada": 3, "description": "", "qtdeEpisodios": 11 }
+      ]
+    },
+    { 
+      "title": "The Good Doctor", 
+      "img": "img/thumbs/gooddoctor.jpg", 
+      "imgCapa": "img/capas/gooddoctor.jpg",
+      "description":"Um jovem médico com autismo começa a trabalhar em um famoso hospital. Além dos desafios da profissão, ele terá também que provar sua capacidade a seus colegas e superiores." , 
+      "temporadas": [
+        { "numeroTemporada": 1, "description": "", "qtdeEpisodios": 9 },
+        { "numeroTemporada": 2, "description": "", "qtdeEpisodios": 10 },
+        { "numeroTemporada": 3, "description": "", "qtdeEpisodios": 11 }
+      ]
+    },
+    { 
+      "title": "Dr. House", 
+      "img": "img/thumbs/house.jpg", 
+      "imgCapa": "img/capas/house.jpg",
+      "description":"O cientista House (Hugh Laurie) é especializado em infectologia e nefrologia, realiza excelentes diagnósticos mantendo uma postura cética, distanciada e até mesmo mau humorada com os pacientes do fictício hospital de Princeton-Plainsboro." , 
+      "temporadas": [
+        { "numeroTemporada": 1, "description": "", "qtdeEpisodios": 9 },
+        { "numeroTemporada": 2, "description": "", "qtdeEpisodios": 10 },
+        { "numeroTemporada": 3, "description": "", "qtdeEpisodios": 11 }
+      ]
+    },
+    { 
+      "title": "Lei & Ordem", 
+      "img": "img/thumbs/leieordem.jpg", 
+      "imgCapa": "img/capas/leieordem.jpg",
+      "description":"Detetives que fazem parte da Unidade de Vítimas Especiais da polícia de Nova York investigam crimes de natureza sexual, como estupros, em que a vítima sobrevive, e auxilia as autoridades na investigação." , 
+      "temporadas": [
+        { "numeroTemporada": 1, "description": "", "qtdeEpisodios": 9 },
+        { "numeroTemporada": 2, "description": "", "qtdeEpisodios": 10 },
+        { "numeroTemporada": 3, "description": "", "qtdeEpisodios": 11 }
+      ]
+    },
+    { 
+      "title": "Loki", 
+      "img": "img/thumbs/loki.jpg", 
+      "imgCapa": "img/capas/loki.jpg", 
+      "description":"Começando imediatamente depois que Loki rouba o Tesseract (de novo), ele se encontra diante da Autoridade de Variação Temporal, uma organização burocrática que existe fora do tempo e espaço. Forçado a responder por seus crimes contra a linha do tempo, ele recebe uma escolha: Ser deletado da realidade ou ajudar a capturar uma ameaça ainda maior." , 
+      "temporadas": [
+        { "numeroTemporada": 1, "description": "", "qtdeEpisodios": 9 },
+        { "numeroTemporada": 2, "description": "", "qtdeEpisodios": 10 },
+        { "numeroTemporada": 3, "description": "", "qtdeEpisodios": 11 }
+      ]
+    },
+    { 
+      "title": "Lucifer", 
+      "img": "img/thumbs/lucifer.jpg", 
+      "imgCapa": "img/capas/lucifer.jpg",
+      "description":"Entediado com a vida nas trevas, o diabo se muda para Los Angeles, abre um piano-bar e empresta sua sabedoria a uma investigadora de assassinatos." , 
+      "temporadas": [
+        { "numeroTemporada": 1, "description": "", "qtdeEpisodios": 9 },
+        { "numeroTemporada": 2, "description": "", "qtdeEpisodios": 10 },
+        { "numeroTemporada": 3, "description": "", "qtdeEpisodios": 11 }
+      ]
+    },
+    { 
+      "title": "Peaky Blinders", 
+      "img": "img/thumbs/peaky.jpg", 
+      "imgCapa": "img/capas/peaky.jpg",
+      "description":"Thomas Shelby e seus irmãos retornam a Birmingham depois de servir no exército britânico durante a Primeira Guerra Mundial. Os Peaky Blinders, a gangue na qual Thomas é líder, controlam a cidade de Birmingham. Mas, como as ambições de Shelby se estendem para além de Birmingham, ele planeja construir o império de negócios que criou e impedir qualquer um que atrapalhar." , 
+      "temporadas": [
+        { "numeroTemporada": 1, "description": "", "qtdeEpisodios": 9 },
+        { "numeroTemporada": 2, "description": "", "qtdeEpisodios": 10 },
+        { "numeroTemporada": 3, "description": "", "qtdeEpisodios": 11 }
+      ]
+    },
+    { 
+      "title": "Vikings", 
+      "img": "img/thumbs/vikings.jpg", 
+      "imgCapa": "img/capas/vikings.jpg",
+      "description":"Ragnar Lothbrok, um jovem que se considera descendente de Odin, é um guerreiro, fazendeiro e chefe de família. Visionário e sonhando viver diversas aventuras, ele acredita que as riquezas sonhadas por seu povo encontram-se além do mar Báltico. Mas para conseguir conquistá-las ele precisa convencer Earl Haraldson, o líder de seu povo, a permitir que ele e seus guerreiros viagem em direção à Inglaterra." , 
+      "temporadas": [
+        { "numeroTemporada": 1, "description": "", "qtdeEpisodios": 9 },
+        { "numeroTemporada": 2, "description": "", "qtdeEpisodios": 10 },
+        { "numeroTemporada": 3, "description": "", "qtdeEpisodios": 11 }
+      ]
+    }
+];
+
+function carregaPrincipal(index){
+    document.querySelector(".filme-principal .titulo").innerHTML = filmes[index].title;
+    document.querySelector(".filme-principal .descricao").innerHTML = filmes[index].description;
+    document.querySelector(".filme-principal").style.background = 
+        `linear-gradient(rgba(0,0,0,0.50),rgba(0,0,0,0.50)100%), url(${filmes[index].imgCapa})`;
+    document.querySelector(".filme-principal").style.backgroundSize = `cover`;
+    document.querySelector(".filme-principal").style.backgroundPosition = `0%0%`;  
+    document.querySelector(".filme-principal").style.transition = `.8s ease all`;
+}
+
 $('.owl-carousel').owlCarousel({
+
     loop:true,
     margin:10,
     nav:false,
@@ -14,3 +149,30 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+filmes.forEach( (filme, index) => {
+    $('.owl-carousel').trigger(
+        'add.owl.carousel', 
+        [
+            $('<div class="item"><img class="box-filme" src="'+filme.img+'" onclick="carregaPrincipal('+index+')" alt="Loki"></div>'), 0
+        ])
+        .trigger('refresh.owl.carousel');
+})
+
+
+const btn = document.getElementById('btnAssistirAgora');
+
+btn.addEventListener('click', ()=>{
+    alert('oi')
+})
+
+function getRandomInt(max) {
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max)).toFixed(0);
+}
+
+const indexInicial = getRandomInt(filmes.length);
+carregaPrincipal(indexInicial);
+
+
+let btnImgs = Array.from(document.querySelectorAll('.box-filme'));
+
